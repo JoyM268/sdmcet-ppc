c1590318c21d2ee13dae7bdfa3d3875156928142
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-02-13 06:45:23 +0530

    Update Dec2Bin.c

--- Extras/Dec2Bin.c
+++ Extras/Dec2Bin.c
@@ -2,25 +2,22 @@
 
 void decimal_to_binary(int n)
 {
-    // array to store binary number
     int binary_num[1000];
 
-    // counter for binary array
     int i = 0;
-    while (n > 0) {
+    while (n > 0)
+    {
 
-        // storing remainder in binary array
         binary_num[i] = n % 2;
         n = n / 2;
         i++;
     }
 
-    // printing binary array in reverse order
     for (int j = i - 1; j >= 0; j--)
         printf("%d", binary_num[j]);
 }
 
-int main()
+main()
 {
     int n;
     printf("Enter a decimal number: ");
@@ -29,5 +26,4 @@ int main()
     printf("Binary equivalent of %d is ", n);
     decimal_to_binary(n);
 
-    return 0;
 }
