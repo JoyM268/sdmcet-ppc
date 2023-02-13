8c4999ed642cbed0de450abf6b692bacab423a5d
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-02-13 06:37:56 +0530

    Add files via upload

---
+++ Pattern Programs/Pattern1.c
@@ -0,0 +1,23 @@
+/* Program to print the following pattern using loops for n diff rows
+example:
+1
+1 2
+1 2 3
+1 2 3 4
+when rows = 4 */
+#include<stdio.h>
+
+main()
+{
+  int rows;
+  printf("Enter the number of rows: ");
+  scanf("%i",&rows);
+  for(int i = 1; i <= rows; i++)
+  {
+    for(int j = 1; j <= i; j++)
+    {
+      printf("%i\t",j);
+    }
+    printf("\n");
+  }
+}
