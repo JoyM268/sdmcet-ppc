ec5b27e3eafcb2f7bf0cd240439ce2515fff399e
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-02-14 05:07:09 +0530

    Update FibonacciSeries.c

--- Loops/FibonacciSeries.c
+++ Loops/FibonacciSeries.c
@@ -9,27 +9,31 @@ main()
   int firstterm = 0;
   int secondterm = 1;
   int thirdterm;
-  if(n != 0)
+
+
+  if(n == 1)
   {
-    if(n == 1)
-    {
-      printf("0\n");
-    }
-    else
-    {
-      printf("%i\n",firstterm);
-      printf("%i\n",secondterm);
-      for(int i = 3; i <= n; i++)
-      {
-        thirdterm = firstterm+secondterm;
-        printf("%i\n",thirdterm);
-        firstterm=secondterm;
-        secondterm=thirdterm;
-      }
-    }
+    printf("0\n");
   }
   else
   {
-    return 0;
+    printf("%i\n",firstterm);
+    printf("%i\n",secondterm);
+    for(int i = 3; i <= n; i++)
+    {
+      thirdterm = firstterm+secondterm;
+      printf("%i\n",thirdterm);
+      firstterm=secondterm;
+      secondterm=thirdterm;
+    }
   }
 }
+  
+  
+  
+  
+  
+
+
+
+  
