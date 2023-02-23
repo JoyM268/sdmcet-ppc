85308eba3147b4fd39f91d3bd0016969d928f19e
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-02-23 21:52:26 +0530

    Add files via upload

---
+++ Arrays/Mean_Variance_SD.c
@@ -0,0 +1,36 @@
+/* Program to read n different numbers find and print
+their mean, variance and standard deviation */
+#include <stdio.h>
+#include <math.h>
+
+main() 
+{
+   int n, i;
+   float sum = 0, mean, variance, std_dev;
+
+   printf("Enter the number of elements: ");
+   scanf("%i", &n);
+
+   int arr[n];
+
+   printf("Enter %i elements:\n", n);
+   for (i = 0; i < n; i++) 
+   {
+      scanf("%i", &arr[i]);
+      sum += arr[i];
+   }
+
+   mean = sum / n;
+   sum = 0;
+   for (i = 0; i < n; i++) 
+   {
+      sum += pow(arr[i] - mean, 2);
+   }
+
+   variance = sum / n;
+   std_dev = sqrt(variance);
+
+   printf("Mean = %.2f\n", mean);
+   printf("Variance = %.2f\n", variance);
+   printf("Standard deviation = %.2f\n", std_dev);
+}
