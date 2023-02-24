84c86f8daacc820675a94bbbb0b85e80a1394e42
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-02-24 18:12:15 +0530

    Update NoKeyDecipher.c

--- Extras/NoKeyDecipher.c
+++ Extras/NoKeyDecipher.c
@@ -5,9 +5,14 @@ possible*/
 #include<string.h>
 main()
 {
-  char alph[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  char alph[26], ch = 'a'; 
   char ciphertext[1000];
   int key,D;
+  for(int i = 0; i < 26; i++)
+  {
+    alph[i] = ch;
+    ch++;
+  }
   printf("Enter ciphertext: ");
   scanf("%s", ciphertext);
   for(int key = 1; key < 26; key ++)
