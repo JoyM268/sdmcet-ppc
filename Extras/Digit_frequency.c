28a03f0a3a0b8d1a576a494b9d38afc858c68b3d
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-02-27 11:20:29 +0530

    Add files via upload

---
+++ Extras/Digit_frequency.c
@@ -0,0 +1,30 @@
+/* Program to count frequency of digits in an integer */
+#include<stdio.h>
+#define base 10
+
+main()
+{
+  int freq[base],num,remainder;
+  printf("Enter a integer: ");
+  scanf("%i",&num);
+  for(int i = 0; i < base; i++)
+  {
+      freq[i] = 0;
+  }
+  while(num > 0)
+  {
+    remainder = num % 10;
+    for(int i = 0; i < base; i++)
+    {
+      if(i == remainder)
+      {
+        freq[i]++;
+      }
+    }
+    num = num / 10;
+  }
+  for(int i = 0; i < base; i++)
+  {
+    printf("The frequency of %i = %i\n",i,freq[i]);
+  }
+}
