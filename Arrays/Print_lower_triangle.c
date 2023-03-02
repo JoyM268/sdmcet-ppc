5615c6cbdc2443662fc98d3682e988390d6f8fef
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-02 22:59:16 +0530

    Add files via upload

---
+++ Arrays/Print_lower_triangle.c
@@ -0,0 +1,36 @@
+/* Program to print all the elements that are present in
+lower triangle of square matrix */
+#include<stdio.h>
+#include<stdlib.h>
+#define ROW 10
+#define COL 10
+main()
+{
+  int arr[ROW][COL],i,j,m,n;
+  printf("Enter the order of the matrix: ");
+  scanf("%i %i",&m,&n);
+  if(m != n)
+  {
+    printf("It is not a square matrix\n");
+    exit(0);
+  }
+  printf("Enter %i elements:\n",m*n);
+  for(i = 0; i < m; i++)
+  {
+    for(j = 0; j < n; j++)
+    {
+      scanf("%i",&arr[i][j]);
+    }
+  }
+  printf("The elements in the lower triangle are:\n");
+  for(i = 0; i < m; i++)
+  {
+    for(j = 0; j < n; j++)
+    {
+      if(i > j)
+      {
+        printf("%i ",arr[i][j]);
+      }
+    }
+  }
+}
