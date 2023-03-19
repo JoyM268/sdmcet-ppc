edf18cceca760477801a6daca5c2f72b88cc07b2
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-19 08:24:51 +0530

    Add files via upload

---
+++ Strings/Concatenate_strings.c
@@ -0,0 +1,22 @@
+/* Program to concatenate two strings */
+#include<stdio.h>
+#define SIZE 40
+
+main()
+{
+  char str1[SIZE],str2[SIZE],str3[SIZE],i;
+  printf("Enter two input strings: ");
+  scanf("%s %s",str1,str2);
+  int j = 0;
+  for(i = 0; str1[i] != '\0'; i++)
+  {
+    str3[j++] = str1[i];
+  }
+  str3[j++] = ' ';
+  for(i = 0; str2[i] != '\0'; i++)
+  {
+    str3[j++] = str2[i];
+  }
+  str3[j] = '\0';
+  printf("Concatenated string: %s",str3);
+}
