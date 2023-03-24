4913f21746e7be8524638d9bf2229daf71a8fc34
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-24 17:38:10 +0530

    Update Q08.c

--- IMP Questions/Part B/Q08.c
+++ IMP Questions/Part B/Q08.c
@@ -10,7 +10,7 @@ main()
   char str1[SIZE],str2[SIZE],i;
   printf("Enter input strings: ");
   scanf("%s",str1);
-  int len = strlen(str1);
+  for(len = 0; str[Len] != '\0'; len++);
   int j = 0;
   for(i = len - 1; i >= 0; i--)
   {
