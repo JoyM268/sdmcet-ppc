08a5ae6c4999664ca683c08bc5f37b2c4fa846b4
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-28 19:38:10 +0530

    Add files via upload

---
+++ IMP Questions/Part B/B2/Q04.c
@@ -0,0 +1,44 @@
+/* Write a C program to search an element from list of 
+‘n’ array elements using binarysearch technique */
+#include<stdio.h>
+#define SIZE 50
+
+ main()
+ {
+   int i,n,found = 0,key,arr[SIZE];
+   printf("Enter the number of elements: ");
+   scanf("%i",&n);
+   printf("Enter %i elements:\n",n);
+   for(i = 0; i < n; i++)
+   {
+     scanf("%i",&arr[i]);
+   }
+   printf("Enter the key element: ");
+   scanf("%i",&key);
+   int low = 0, high = n - 1,mid;
+   while(low <= high)
+   {
+     mid = (low + high) / 2;
+     if(arr[mid] == key)
+     {
+       found = 1;
+       break;
+     }
+     else if(arr[mid] > key)
+     {
+       high = mid - 1;
+     }
+     else
+     {
+       low = mid + 1;
+     }
+   }
+   if(found == 1)
+   {
+     printf("%i found at position %i\n",key,mid);
+   }
+   else
+   {
+     printf("%i not found\n",key);
+   }
+ } 
