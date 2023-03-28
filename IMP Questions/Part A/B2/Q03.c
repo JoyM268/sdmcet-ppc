6ac7d6b32831b43b424f846b0ef82945fc2f3891
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-28 17:45:55 +0530

    Add files via upload

---
+++ IMP Questions/Part A/B2/Q03.c
@@ -0,0 +1,39 @@
+/* Write a C program to print the following patterns:
+a                    $ $ $
+a b                    $ $
+a b c                    $
+.....                          For ‘n’ rows */
+
+#include<stdio.h>
+
+main()
+{
+  int rows,i,j;
+  printf("Enter the number of rows: ");
+  scanf("%i",&rows);
+  for(i = 0; i < rows; i++)
+  {
+    char ch = 'a';
+    for(j = 0; j <= i; j++)
+    {
+      printf("%c\t",ch++);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  for(i = 0; i < rows; i++)
+  {
+    for(j = 0; j < rows; j++)
+    {
+      if(i > j)
+      {
+        printf(" \t");
+      }
+      else
+      {
+        printf("$\t");
+      }
+    }
+    printf("\n");
+  }
+}
