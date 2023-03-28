50950aa627aaa67150c1fe7b531ed6ebba2a2e06
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-28 19:43:01 +0530

    Add files via upload

---
+++ IMP Questions/Part B/B2/Q06.c
@@ -0,0 +1,30 @@
+/* Write a C program that computes the mean, variance and
+standard deviation of nelements in an array */
+#include <stdio.h>
+#include <math.h>
+#define SIZE 50
+main() 
+{
+   int n, i,arr[SIZE];
+   float sum = 0, mean, variance, std_dev;
+   printf("Enter the number of elements: ");
+   scanf("%i", &n);
+   printf("Enter %i elements:\n", n);
+   for (i = 0; i < n; i++) 
+   {
+      scanf("%i", &arr[i]);
+      sum += arr[i];
+   }
+   mean = sum / n;
+   sum = 0;
+   for(i = 0; i < n; i++) 
+   {
+      sum += pow(arr[i] - mean, 2);
+   }
+   variance = sum / n;
+   std_dev = sqrt(variance);
+
+   printf("Mean = %f\n", mean);
+   printf("Variance = %f\n", variance);
+   printf("Standard deviation = %f\n", std_dev);
+}
