04d2a9f3da5a07b86045ed58cd3bd87b59433f68
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-30 16:57:52 +0530

    Rename Q04.c to Q04.c

---
+++ IMP Questions/Batch 2/Part A/Q04.c
@@ -0,0 +1,15 @@
+/* Write a C program to print the value of following sum:
+Sum = 1 + 3 + 5 + .... */
+#include <stdio.h>
+
+main() 
+{
+   int n, i, sum = 0;
+   printf("Enter the limit: ");
+   scanf("%i", &n);
+   for (i = 1; i <= n; i += 2) 
+   {
+      sum += i;
+   }
+   printf("Sum = %i\n",sum);
+}
