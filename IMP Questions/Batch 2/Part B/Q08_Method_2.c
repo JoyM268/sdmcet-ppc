89bd5f8687288f42e3b9f7290a04d2a56cfd4daa
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-03-31 07:09:50 +0530

    Add files via upload

---
+++ IMP Questions/Batch 2/Part B/Q08_Method_2.c
@@ -0,0 +1,68 @@
+/* Write a C program to read 2 strings, compare both of 
+them. If both are unequal then concatenate the 1st string 
+with the 2nd and print the resultant string; if equal 
+print the success message along with the length of the 
+strings. */
+/* Method 2 */  
+#include<stdio.h>
+#define SIZE 50
+
+int compare(char[],char[]);
+void concatenate(char[],char[],char[]);
+main() 
+{
+  char str1[SIZE],str2[SIZE];
+  printf("Enter input string 1: ");
+  scanf("%[^\n]",str1);
+  getchar();
+  printf("Enter input string 2: ");
+  scanf("%[^\n]",str2);
+  if(compare(str1,str2) == 0)
+  {
+    int len;
+    for(len = 0; str1[len] != '\0'; len++);
+    printf("Both the strings are equal\n");
+    printf("The lenght of the string is %i\n",len);
+  }
+  else
+  {
+    char str3[2*SIZE];
+    concatenate(str1,str2,str3);
+    printf("Resultant String: %s\n",str3);
+  }
+}
+int compare(char str1[SIZE],char str2[SIZE])
+{
+  int i;
+  int len1,len2;
+  for(len1 = 0; str1[len1] != '\0'; len1++);
+  for(len2 = 0; str2[len2] != '\0'; len2++);
+  if(len1 != len2)
+  {
+    return 1;
+  }
+  else
+  {
+    for(i = 0; str1[i] != '\0'; i++)
+    {
+      if(str1[i] != str2[i])
+      {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+void concatenate(char str1[SIZE],char str2[SIZE],char str3[2 * SIZE])
+{
+    int i, j = 0;
+    for (i = 0; str1[i] != '\0'; i++)
+    {
+        str3[j++] = str1[i];
+    }
+    for (i = 0; str2[i] != '\0'; i++)
+    {
+        str3[j++] = str2[i];
+    }
+    str3[j] = '\0';
+}
