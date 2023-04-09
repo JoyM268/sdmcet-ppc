1984e879ed0f2bef882c102bb4620da2f064749f
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-04-09 12:40:39 +0530

    Update Swapping2.c

--- Simple Programs/Swapping2.c
+++ Simple Programs/Swapping2.c
@@ -8,9 +8,11 @@ main()
   int a,b;
   printf("Enter two numbers: ");
   scanf("%i %i",&a,&b);
-  printf("Before Swap:\nThe value of a is %i and b is %i\n",a,b);
+  printf("Before Swap:\n");
+  printf("The value of a is %i and b is %i\n",a,b);
   a=a+b;
   b=a-b;
   a=a-b;
-  printf("After Swap:\nThe value of a is %i and b is %i\n",a,b);
+  printf("After Swap:\n");
+  printf("The value of a is %i and b is %i\n",a,b);
 }
