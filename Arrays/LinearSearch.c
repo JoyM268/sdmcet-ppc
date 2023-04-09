87a57be17ee3d5c02f20f0742cea6989dd6d79c6
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-04-09 05:42:39 +0530

    Update LinearSearch.c

--- Arrays/LinearSearch.c
+++ Arrays/LinearSearch.c
@@ -1,12 +1,11 @@
 /* Linear Search */
 #include<stdio.h>
-
+#define SIZE 50
 main()
 {
-  int n, found = 0,key,i;
+  int n, found = 0,key,i,arr[SIZE];
   printf("Enter number of elements: ");
   scanf("%i",&n);
-  int arr[n];
   printf("Enter %i elements:\n",n);
   for(i = 0; i < n; i++)
   {
