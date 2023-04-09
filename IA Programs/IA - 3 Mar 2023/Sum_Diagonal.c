40f2dcecc54825b3395d7230a63bb3ee0342ceec
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-04-09 22:06:13 +0530

    Add files via upload

---
+++ IA Programs/IA - 3 Mar 2023/Sum_Diagonal.c
@@ -0,0 +1,34 @@
+#include<stdio.h> 
+ #include<stdlib.h> 
+ #define ROW 10 
+ #define COL 10 
+ main() 
+ { 
+   int arr[ROW][COL],i,j,m,n,sum = 0; 
+   printf("Enter the order of the matrix: "); 
+   scanf("%i %i",&m,&n); 
+   if(m != n) 
+   { 
+     printf("It is not a square matrix\n"); 
+     exit(0); 
+   } 
+   printf("Enter %i elements:\n",m*n); 
+   for(i = 0; i < m; i++) 
+   { 
+     for(j = 0; j < n; j++) 
+     { 
+       scanf("%i",&arr[i][j]); 
+     } 
+   }  
+   for(i = 0; i < m; i++) 
+   { 
+     for(j = 0; j < n; j++) 
+     { 
+       if(i == j) 
+       { 
+         sum += arr[i][j];
+       } 
+     } 
+   }
+   printf("The sum of principal diagonal elements is: %i\n",sum);
+ }
