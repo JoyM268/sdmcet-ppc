91e8d16dcfac57fa4817b7e8cef7fc15c5323e03
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-04-10 17:25:21 +0530

    Update Ternary6.c

--- Ternary Operator/Ternary6.c
+++ Ternary Operator/Ternary6.c
@@ -7,6 +7,6 @@ main()
   int a,b,c,largest;
   printf("Enter 3 numbers: ");
   scanf("%i %i %i",&a,&b,&c);
-  largest = (a>b)? a : ((b>c)? b : c);
+  largest = (a>b)? ((a>c)? a : c) : ((b>c)? b : c);
   printf("The largest of the 3 numbers is: %i", largest);
 }
